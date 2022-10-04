75518eefc3e483d1369f9216342af7cfefccc2d6
MaksShostak <107349913+MaksShostak@users.noreply.github.com>
2022-10-04 21:31:00 +0300

    Update ButtonGoBack.jsx

--- src/components/Button/ButtonGoBack.jsx
+++ src/components/Button/ButtonGoBack.jsx
@@ -4,7 +4,7 @@ import { IoArrowBackSharp } from 'react-icons/io5';
 import { ButtonLink, Button } from './ButtonGoBack.styled';
 
 const ButtonGoBack = ({ location }) => {
-  console.log(location);
+  // console.log(location);
   return (
     <ButtonLink to={location.state?.from ?? `/movies`}>
       <Button type="button">
